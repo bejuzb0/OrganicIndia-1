dbe742c3d2b236f6b7c911a79e62accc31d8fba9
vedant-g1 <vedant.golhani@gmail.com>
2020-04-23 11:40:54 +0530

    first commit

--- app/src/main/java/com/example/organicindiapre/MainActivity.java
+++ app/src/main/java/com/example/organicindiapre/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
     private Button Login;
     private TextView Sign;
    // private Button button;
-//some change
+//testing
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
