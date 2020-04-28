3b226f45341c6c83e6a9d85a1b3ffc7616c75c12
Aditya Kumar <bejuzb0@gmail.com>
2020-04-28 16:59:17 +0530

    Vendor Product

--- app/src/main/java/com/example/organicindiapre/ItemAdapter.java
+++ app/src/main/java/com/example/organicindiapre/ItemAdapter.java
@@ -6,6 +6,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 
+import com.example.organicindiapre.customer.CustProduct_Subclass;
 import com.example.organicindiapre.customer.CustomerClass;
 
 import java.util.List;
@@ -36,20 +37,23 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
 
     @Override
     public void onBindViewHolder(@NonNull ItemViewHolder holder, int position) {
-        CustomerClass customerObj =itemList.get(position);
-        holder.cust_name.setText(customerObj.getCust_name());
-        holder.cust_phone_no.setText(customerObj.getPhone_no());
-        holder.cust_address.setText(customerObj.getAddress());
-        LinearLayoutManager layoutManager = new LinearLayoutManager(
-                holder.productRecyclerView.getContext(),
-                LinearLayoutManager.VERTICAL,
-                false
-        );
-        layoutManager.setInitialPrefetchItemCount(customerObj.getProductList().size());
-        SubItemAdapter subItemAdapter = new SubItemAdapter(customerObj.getProductList());
-        holder.productRecyclerView.setLayoutManager(layoutManager);
-        holder.productRecyclerView.setAdapter(subItemAdapter);
-        holder.productRecyclerView.setRecycledViewPool(viewPool);
+            CustomerClass customerObj = itemList.get(position);
+            //if(customerObj != null) {
+                holder.cust_name.setText(customerObj.getCust_name());
+                holder.cust_phone_no.setText(customerObj.getPhone_no());
+                holder.cust_address.setText(customerObj.getAddress());
+                LinearLayoutManager layoutManager = new LinearLayoutManager(
+                        holder.productRecyclerView.getContext(),
+                        LinearLayoutManager.VERTICAL,
+                        false
+                );
+                layoutManager.setInitialPrefetchItemCount(customerObj.getProductList().size());
+                SubItemAdapter subItemAdapter = new SubItemAdapter(customerObj.getProductList());
+                holder.productRecyclerView.setLayoutManager(layoutManager);
+                holder.productRecyclerView.setAdapter(subItemAdapter);
+                holder.productRecyclerView.setRecycledViewPool(viewPool);
+            //}
+
 
     }
 
@@ -71,4 +75,45 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemViewHolder
             productRecyclerView = itemView.findViewById(R.id.recycViewProductList);
         }
     }
+
+    public class SubItemAdapter extends RecyclerView.Adapter<SubItemAdapter.SubItemViewHolder> {
+        List<CustProduct_Subclass> subItemList;
+
+        SubItemAdapter(List<CustProduct_Subclass> subItemList) {
+            this.subItemList = subItemList;
+        }
+
+        @NonNull
+        @Override
+        public SubItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.cust_product_row_0, parent, false);
+            return new SubItemViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull SubItemViewHolder holder, int position) {
+            CustProduct_Subclass subItem = subItemList.get(position);
+            holder.prod_name.setText(subItem.getProductName());
+            holder.prod_qty.setText(subItem.getQuantity());
+            holder.prod_amnt.setText(subItem.getAmount());
+        }
+
+        @Override
+        public int getItemCount() {
+            return subItemList.size();
+        }
+
+        public class SubItemViewHolder extends RecyclerView.ViewHolder {
+            TextView prod_name;
+            TextView prod_qty;
+            TextView prod_amnt;
+            public SubItemViewHolder(@NonNull View itemView) {
+                super(itemView);
+                prod_name = itemView.findViewById(R.id.product_name);
+                prod_amnt = itemView.findViewById(R.id.amount_order);
+                prod_qty = itemView.findViewById(R.id.quantity_order);
+            }
+        }
+    }
+
 }
